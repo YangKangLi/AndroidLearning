6e62080c92028e48d17138afbc2e7502ceb9248e
YangKangli <yangkl@ziquan.com.cn>
2018-01-24 17:32:31 +0800

    下班提交代码（第五节待完成）

--- Lesson4/src/main/java/cn/com/ziquan/android/learning/lesson4/section5/Section5Activity.java
+++ Lesson4/src/main/java/cn/com/ziquan/android/learning/lesson4/section5/Section5Activity.java
@@ -11,5 +11,8 @@ public class Section5Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section5);
+
+        //
+        //
     }
 }
